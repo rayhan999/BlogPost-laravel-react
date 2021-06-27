d524ce3e951437e7f0423c12e83ecd733e31ac95
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-06-27 12:57:12 +0600

    procfile added

--- routes/api.php
+++ routes/api.php
@@ -22,20 +22,20 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 Route::post('/logincheck', [loginController::class, 'verify']);
-Route::post('register', [loginController::class, 'register']);
+Route::post('/register', [loginController::class, 'register']);
 
-Route::get('posts', [PostController::class, 'index']);
-Route::get('posts/{id}', [PostController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::get('/users/posts/{uname}', [PostController::class, 'userposts']);
 
-Route::get('myposts', [MyPostController::class, 'index']);
-Route::post('addpost', [MyPostController::class, 'store']);
-Route::delete('myposts/delete/{id}', [MyPostController::class, 'destroy']);
-Route::get('myposts/edit/{id}', [MyPostController::class, 'edit']);
-Route::post('myposts/edit/{id}', [MyPostController::class, 'update']);
+Route::get('/myposts', [MyPostController::class, 'index']);
+Route::post('/addpost', [MyPostController::class, 'store']);
+Route::delete('/myposts/delete/{id}', [MyPostController::class, 'destroy']);
+Route::get('/myposts/edit/{id}', [MyPostController::class, 'edit']);
+Route::post('/myposts/edit/{id}', [MyPostController::class, 'update']);
 
-Route::post('posts/addcomment', [CommentController::class, 'store']);
-Route::get('posts/comments/{id}', [CommentController::class, 'show']);
+Route::post('/posts/addcomment', [CommentController::class, 'store']);
+Route::get('/posts/comments/{id}', [CommentController::class, 'show']);
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
