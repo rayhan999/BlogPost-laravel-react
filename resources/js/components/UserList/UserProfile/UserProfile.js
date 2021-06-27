0dc2d05a9b7e6df3f56cb7568509a07916032352
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-06-27 11:36:10 +0600

    Users posts update

--- resources/js/components/UserList/UserProfile/UserProfile.js
+++ resources/js/components/UserList/UserProfile/UserProfile.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from "react";
+import PaginationComponent from "react-bootstrap/Pagination";
 import { useHistory, useParams, Link } from 'react-router-dom';
 import Navbar from '../../Navbar/Navbar';
 import UserPosts from './UserPosts';
@@ -7,36 +8,76 @@ const UserProfile = () => {
     const { id } = useParams();
     const [user, setUser] = useState();
     // console.log("usersssss", user);
-    
+    const [posts, setPosts] = useState([]);
+    const [mount, setMount] = useState(true);
+    const [totalItems, setTotalItems] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [ItemsPerPage, setItemsPerPage] = useState(5);
+    const [totalPages, setTotalPages] = useState(0);
     useEffect(() => {
-        const getData = async () => {
-            await axios.get(`/api/users/${id}`)
-                .then(res => {
-                    setUser(res.data[0]);
-                    // console.log("user", user);
 
+        axios.get(`/api/users/${id}`)
+            .then(res => {
+                setUser(res.data[0]);
+                if (!user) {
+                    setMount(!mount);
+                } else {
+                    return user;
+                }
+                // console.log("user", res.data[0]);
+
+
+            })
+            .catch(error => console.log(error.message));
+
+
+        if (user) {
+            // console.log("u", user);
+            axios.get(`/api/users/posts/${user.uname}`)
+                .then(res => {
+                    setPosts(res.data);
+                    // console.log("posts", res.data);
 
                 })
                 .catch(error => console.log(error.message));
-
         }
-        getData();
-        // if (user) {
+        if (totalItems > 0 && ItemsPerPage > 0) {
+            setTotalPages(Math.ceil(totalItems / ItemsPerPage));
+        }
+    }, [mount, totalItems, ItemsPerPage]);
 
-        // }
-    }, []);
-    // const handlePrint = () => {
-    //     if (user) {
-    //         // console.log("u", user.uname);
-    //         axios.get(`/api/users/posts/${user.uname}`)
-    //             .then(res => {
-    //                 setPosts(res.data);
-    //                 // console.log("posts", res.data);
+    const commentsData = useMemo(() => {
+        let computedComments = posts;
 
-    //             })
-    //             .catch(error => console.log(error.message))
-    //     }
-    // }
+
+        setTotalItems(computedComments.length);
+
+
+
+        //Current Page slice
+        return computedComments.slice(
+            (currentPage - 1) * ItemsPerPage,
+            (currentPage - 1) * ItemsPerPage + ItemsPerPage
+        );
+    }, [posts, currentPage, ItemsPerPage]);
+
+    const paginationItems = useMemo(() => {
+        const pages = [];
+
+        for (let i = 1; i <= totalPages; i++) {
+            pages.push(
+                <PaginationComponent.Item
+                    key={i}
+                    active={i == currentPage}
+                    onClick={() => setCurrentPage(i)}
+                >
+                    {i}
+                </PaginationComponent.Item>
+            );
+        }
+
+        return pages;
+    }, [totalPages, currentPage]);
 
     return (
         <div>
@@ -67,7 +108,46 @@ const UserProfile = () => {
                             </div>
                         </div>
                         <div>
-                            <UserPosts uname={user.uname}></UserPosts>
+                            {/* <UserPosts uname={user.uname}></UserPosts> */}
+                            <div>
+                                <h1>posts</h1>
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">First</th>
+                                            <th scope="col">Last</th>
+                                            {/* <th scope="col">Handle</th> */}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        {commentsData.map(post => (
+                                            <tr key={post.id}>
+                                                <th scope="row">{post.id}</th>
+                                                <td><Link to={`/posts/${post.id}`} style={{ textDecoration: "none", color: "black" }}>{post.title}</Link></td>
+                                                <td>{post.description.slice(0,50)}...</td>
+                                                {/* <td>@mdo</td> */}
+                                            </tr>
+                                        ))}
+
+
+                                    </tbody>
+                                </table>
+                                <div className="d-flex justify-content-end">
+                                    <PaginationComponent>
+                                        <PaginationComponent.Prev
+                                            onClick={() => setCurrentPage(currentPage - 1)}
+                                            disabled={currentPage === 1}
+                                        />
+                                        {paginationItems}
+                                        <PaginationComponent.Next
+                                            onClick={() => setCurrentPage(currentPage + 1)}
+                                            disabled={currentPage === totalPages}
+                                        />
+                                    </PaginationComponent>
+                                </div>
+                            </div>
                         </div>
                     </>
                 }
